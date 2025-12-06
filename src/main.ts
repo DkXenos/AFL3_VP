369a1441e3074b071fbb1c74e6f5ca1242473a6d
DkXenos <superjason101010@gmail.com>
2025-12-06 15:31:14 +0700

    ready for correction

--- src/main.ts
+++ src/main.ts
@@ -13,4 +13,5 @@ app.use(errorMiddleware);
 
 app.listen(PORT || 3000, () => {
     console.log('Connected yeay')
+    console.log(`aku telah buatkan sebuah seeder jadi bisa menggunakan data dummmy yang saya seed`);
 });
